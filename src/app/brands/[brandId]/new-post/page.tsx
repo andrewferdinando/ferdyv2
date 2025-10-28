4c2dfc60ecec8f643ed2cecb2a455580ef1e7bad
andrewferdinando <andrew@adhoc.help>
2025-10-29 09:05:17 +1300

    Improve icon vertical centering in date and time inputs
    - Add small negative margin-top to better center icons
    - Use Tailwind translate classes directly instead of transform utility
    - Ensures icons are perfectly centered vertically in input boxes

--- src/app/brands/[brandId]/new-post/page.tsx
+++ src/app/brands/[brandId]/new-post/page.tsx
@@ -422,7 +422,7 @@ export default function NewPostPage() {
                               MozAppearance: 'textfield'
                             }}
                           />
-                          <svg className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400 pointer-events-none" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                          <svg className="absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400 pointer-events-none" fill="none" stroke="currentColor" viewBox="0 0 24 24" style={{ marginTop: '-1px' }}>
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
                           </svg>
                         </div>
@@ -442,7 +442,7 @@ export default function NewPostPage() {
                               MozAppearance: 'textfield'
                             }}
                           />
-                          <svg className="absolute right-3 top-1/2 transform -translate-y-1/2 w-4 h-4 text-gray-400 pointer-events-none" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                          <svg className="absolute right-3 top-1/2 -translate-y-1/2 w-4 h-4 text-gray-400 pointer-events-none" fill="none" stroke="currentColor" viewBox="0 0 24 24" style={{ marginTop: '-1px' }}>
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
                           </svg>
                         </div>
